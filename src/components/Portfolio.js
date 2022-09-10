d3bd257a16680d8613213e7e8acb82ddadf3b64c
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-09-10 17:20:25 +0200

    added GuitarLA web

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import amigaria from '../images/amigaria.png'
 import apv from '../images/apv.png'
-import tailwind from '../images/tailwind.png'
+import guitarla from '../images/GuitarLA.png'
 import vue from '../images/vue.png'
 import react from '../images/react.png'
 
@@ -63,8 +63,10 @@ const Portfolio = () => {
           </div>
 
           <div className='pb-6 mx-2 py-2'>
-            <img src={tailwind} alt="" className='h-48 w-80 mx-auto my-2 shadow-lg rounded-md hover:scale-105 hover:cursor-pointer transition-all duration-500 ease-in-out' />
-            <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <a href="https://flourishing-lolly-cf3644.netlify.app" target={'_blank'} rel="noreferrer">
+              <img src={guitarla} alt="" className='h-48 w-80 mx-auto my-2 shadow-lg rounded-md hover:scale-105 hover:cursor-pointer transition-all duration-500 ease-in-out' />
+              <p className='text-center'>Tienda de guitarras</p>
+            </a>
           </div>
 
           <div className='pb-6 mx-2 py-2'>
