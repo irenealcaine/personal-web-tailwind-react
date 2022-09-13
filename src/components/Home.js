0cbdf1ddfd4d944a0c4cf474df5a743a14461bda
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-09-13 10:59:05 +0200

    added home page styles and content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import arrowDown from '../images/arrowdown.gif'
 
 const Home = () => {
   return (
@@ -6,14 +7,13 @@ const Home = () => {
       <div className='h-screen relative' id='home'>
 
         <h1 className='text-8xl md:text-9xl w-1/2 py-8 px-4 font-caveat'>¡Hola! Soy <span className='text-violet-600'>Irene Alcaine</span></h1>
-        <p>frontend developer</p>
-        <p>Bienvenido a mi web</p>
+        <p className='text-3xl pl-4 mt-10 underline decoration-violet-600 decoration-4 underline-offset-8'>Frontend developer</p>
 
         <div className='absolute top-10 right-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-overlay fliter blur-md animate-blob'></div>
         <div className='absolute top-24 right-44 w-72 h-72 bg-sky-300 rounded-full mix-blend-overlay fliter blur-md animate-blob animation-delay-2000'></div>
         <div className='absolute top-56 right-20 w-72 h-72 bg-violet-300 rounded-full mix-blend-overlay fliter blur-md animate-blob animation-delay-4000'></div>
 
-        <span className='absolute bottom-0'>FlechaAbajo</span>
+        <a href='#portfolio' className='absolute bottom-2 right-3 '><img src={arrowDown} alt="" className='w-40' /></a>
 
       </div>
     </>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,17 +13,17 @@ const Portfolio = () => {
 
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
+
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 4
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 1
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
@@ -44,7 +44,7 @@ const Portfolio = () => {
           swipeable={false}
           draggable={false}
           responsive={responsive}
-          ssr={true} // means to render carousel on server-side.
+          ssr={true}
           infinite={true}
         >
 
