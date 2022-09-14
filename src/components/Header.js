6511c86e8421fba46f6ed50e8c3d294c9e8506f8
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-09-14 16:38:47 +0200

    added some styles to header, again

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,18 +22,18 @@ const Header = () => {
           onClick={() => { setOpen(!open) }}
         />
         <div className={`${open ? '' : 'hidden'} md:flex md:w-1/2 md:justify-end text-xl`}>
-          <div className={`flex flex-col md:flex-row md:items-center pr-7`}>
+          <div className={`flex flex-col md:flex-row md:items-center pr-7 md:mt-0 mt-2`}>
 
-            <a href="#portfolio" className='px-2'>Portfolio </a>
-            <a href="#skills" className='px-2'>Skills</a>
+            <a href="#portfolio" className='px-2 hover:underline'>Portfolio </a>
+            <a href="#skills" className='px-2 hover:underline'>Skills</a>
           </div>
           <div className='md:flex md:items-center'>
-            <div className='flex items-center'>
-              <a href="https://www.github.com/irenealcaine" target={'_blank'} rel="noreferrer"><img src={github} alt="" className='h-8 mx-1' /></a>
-              <a href="https://www.linkedin.com/in/irenealcaine/" target={'_blank'} rel="noreferrer"><img src={linkedin} alt="" className='h-8 mx-1' /></a>
-              <a href="https://www.codepen.io/ialcaine" target={'_blank'} rel="noreferrer"><img src={codepen} alt="" className='h-8 mx-1' /></a>
+            <div className='flex items-center mx-2 md:mt-0 mt-2'>
+              <a href="https://www.github.com/irenealcaine" target={'_blank'} rel="noreferrer"><img src={github} alt="" className='h-8 hover:filter hover:invert ' /></a>
+              <a href="https://www.linkedin.com/in/irenealcaine/" target={'_blank'} rel="noreferrer"><img src={linkedin} alt="" className='h-8 mx-2 hover:filter hover:invert' /></a>
+              <a href="https://www.codepen.io/ialcaine" target={'_blank'} rel="noreferrer"><img src={codepen} alt="" className='h-8 hover:filter hover:invert' /></a>
             </div>
-            <button className='bg-white text-violet-900 font-bold border-2 border-violet-900 rounded py-1 px-6 mx-2 hover:bg-violet-900 hover:text-white hover:border-white'>Contacto</button>
+            <button className='bg-white text-violet-900 font-bold border-2 border-violet-900 rounded py-1 px-6 mx-2 hover:bg-violet-900 hover:text-white hover:border-white md:mt-0 mt-4'>Contacto</button>
           </div>
         </div>
       </div>
