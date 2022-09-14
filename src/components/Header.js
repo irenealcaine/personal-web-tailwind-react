d467d8e183b09de61d1e06c3790d0a505ecfd7ac
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-09-14 12:42:45 +0200

    added some styles to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,16 +13,16 @@ const Header = () => {
 
   return (
     <>
-      <div className='bg-violet-900 shadow-lg py-4 px-7 md:flex md:justify-between sticky top-0 z-50'>
-        <a href='#home' className='text-2xl font-caveat'>Irene</a>
+      <div className='bg-violet-900 shadow-lg py-4 px-7 md:flex md:justify-between sticky top-0 z-50 font-caveat'>
+        <a href='#home' className='text-3xl '>Irene</a>
         <img
           className='absolute top-2 right-2 md:hidden p-2 w-10 h-10'
           src={`${open ? closeMenu : openMenu}`}
           alt=""
           onClick={() => { setOpen(!open) }}
         />
-        <div className={`${open ? '' : 'hidden'} md:flex md:w-1/3 md:justify-between`}>
-          <div className={`flex flex-col md:flex-row md:items-center`}>
+        <div className={`${open ? '' : 'hidden'} md:flex md:w-1/2 md:justify-end text-xl`}>
+          <div className={`flex flex-col md:flex-row md:items-center pr-7`}>
 
             <a href="#portfolio" className='px-2'>Portfolio </a>
             <a href="#skills" className='px-2'>Skills</a>
