d3a0169fe36734723145678992973e653f3db938
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-16 17:11:03 +0200

    styled skills

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,11 +13,11 @@ const Portfolio = () => {
     superLargeDesktop: {
 
       breakpoint: { max: 4000, min: 3000 },
-      items: 5
+      items: 4
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 4
+      items: 3
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
@@ -34,7 +34,7 @@ const Portfolio = () => {
 
       <div className='md:min-h-screen flex flex-col' id='portfolio'>
         <h2 className='pt-20 text-center text-6xl md:text-8xl text-violet-700 font-caveat mb-8'>Proyectos</h2>
-        <div className='md:pt-20'>
+        <div className='md:mt-8 mt-4 w-5/6 mx-auto'>
           <Carousel
             swipeable={true}
             responsive={responsive}
@@ -52,7 +52,7 @@ const Portfolio = () => {
 
                   <div key={key} className="mx-1">
                     <a href={data.url} target={'_blank'} rel="noreferrer">
-                      <img src={data.img} alt="" className='h-60 mx-auto my-2 shadow-lg rounded-md hover:scale-105 hover:cursor-pointer transition-all duration-500 ease-in-out' />
+                      <img src={data.img} alt="" className='md:h-72 h-60 mx-auto my-2 shadow-lg rounded-md hover:scale-105 hover:cursor-pointer transition-all duration-500 ease-in-out' />
                       <p className='text-center'>{data.title}</p>
                     </a>
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -18,10 +18,10 @@ const Skills = () => {
 
         <div className='my-auto '>
 
-          <div className='flex flex-col md:flex-row'>
+          <div className='flex flex-col md:flex-row w-5/6 mx-auto'>
 
-            <Tabs selectedTabClassName='bg-violet-700 font-bold' className='md:w-1/3'>
-              <TabList className='flex flex-wrap justify-center flex-col md:flex-row bg-violet-900 p-2 rounded-lg  w-5/6 mx-auto bg-opacity-60'>
+            <Tabs selectedTabClassName='bg-violet-700 font-bold' className='md:w-1/3 mr-0 md:mr-8'>
+              <TabList className='flex flex-wrap justify-center flex-col md:flex-row bg-violet-900 p-2 rounded-lg  mx-auto bg-opacity-60'>
                 {skillsData.map((data, key) => {
                   return (
                     <Tab className='rounded-lg py-2 px-4 mx-2 hover:bg-violet-800 hover:cursor-pointer transition-all duration-500 ease-in-out'><h3 className='md:text-xl'>{data.title}</h3></Tab>
@@ -32,7 +32,7 @@ const Skills = () => {
               {skillsData.map((data, key) => {
                 return (
                   <TabPanel>
-                    <div className='flex flex-wrap justify-center border-2 border-violet-900 rounded-xl p-1 w-5/6 mx-auto mt-2'>
+                    <div className='flex flex-wrap justify-center border-2 border-violet-900 rounded-xl p-1 mx-auto mt-2'>
                       {data.icons.map(s => (<img src={s} alt="" className='h-16 m-3' />))}
                     </div>
                   </TabPanel>
@@ -41,13 +41,13 @@ const Skills = () => {
             </Tabs>
 
             <Tabs selectedTabClassName='bg-violet-700 font-bold' className='md:w-2/3'>
-              <TabList className='flex justify-center flex-col md:flex-row bg-violet-900 p-2 rounded-lg mt-4 md:mt-0 w-5/6 mx-auto bg-opacity-60'>
+              <TabList className='flex justify-center flex-col md:flex-row bg-violet-900 p-2 rounded-lg mt-4 md:mt-0 mx-auto bg-opacity-60'>
                 <Tab className='rounded-lg py-2 px-4 mx-2 hover:bg-violet-800 hover:cursor-pointer transition-all duration-500 ease-in-out'><h3 className='md:text-xl'>Formación</h3></Tab>
                 <Tab className='rounded-lg py-2 px-4 mx-2 hover:bg-violet-800 hover:cursor-pointer transition-all duration-500 ease-in-out'><h3 className='md:text-xl'>Experiencia</h3></Tab>
               </TabList>
 
               <TabPanel>
-                <div className='border-2 border-violet-900 rounded-xl w-5/6 mx-auto mt-2 px-4 py-2'>
+                <div className='border-2 border-violet-900 rounded-xl mx-auto mt-2 px-4 py-2'>
                   {educationData.map((data, key) => {
                     return (
 
@@ -70,7 +70,7 @@ const Skills = () => {
                 </div>
               </TabPanel>
               <TabPanel>
-                <div className='border-2 border-violet-900 rounded-xl w-5/6 mx-auto mt-2 px-4 py-2'>
+                <div className='border-2 border-violet-900 rounded-xl mx-auto mt-2 px-4 py-2'>
                   {experienceData.map((data, key) => {
                     return (
 
@@ -95,7 +95,7 @@ const Skills = () => {
             </Tabs>
           </div>
 
-          <a href={CV} download="CV_Irene_Alcaine.pdf"><p className='md:text-xl mt-8 rounded-xl indent-4 md:w-1/2 w-5/6 mx-auto text-violet-300 text-center border-4 border-violet-900 py-2 hover:cursor-pointer hover:bg-violet-900 hover:text-white transition-all duration-500 ease-in-out'>Descargar CV</p></a>
+          <a href={CV} download="CV_Irene_Alcaine.pdf"><p className='md:text-3xl text-xl mt-8 rounded-xl indent-4 md:w-1/2 w-5/6 mx-auto text-violet-300 text-center border-4 border-violet-900 py-2 hover:cursor-pointer hover:bg-violet-900 hover:text-white transition-all duration-500 ease-in-out'>Descargar CV</p></a>
 
         </div>
       </div >
