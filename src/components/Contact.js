4debfa249f1c23a1b2fee1889c9c7d9d8c75921e
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-18 13:42:32 +0200

    contact component styled

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import phone from '../images/phone.png'
+import phone from '../images/whatsapp.png'
 import mail from '../images/mail.png'
 import emailjs from '@emailjs/browser';
 import { useState } from 'react';
@@ -34,54 +34,57 @@ const Contact = () => {
 
   return (
     <>
-      <div id="contact" className='md:min-h-screen flex flex-col'>
+      <div id="contact" className='md:min-h-screen flex flex-col items-center'>
         <h2 className='pt-20 text-center text-6xl md:text-8xl text-violet-700 font-caveat'>Contacto</h2>
-        <div className='md:pt-20'>
-          <p className='text-center text-xl my-2'>¿En qué puedo ayudarte?</p>
-          <div className='flex flex-col px-4 md:px-0 py-4 md:items-start md:flex-row justify-center'>
-            <div className='mx-3 p-1'><img src={mail} alt="" className='h-6 inline-block' /> irenealcainealvarez@gmail.com</div>
-            <div className='mx-3 p-1'><img src={phone} alt="" className='h-6 inline-block' /> +34 629 88 21 29</div>
+        <div className='md:pt-20 flex md:flex-row flex-col justify-center md:w-5/6'>
+          <div className='md:w-2/5'>
+            <p className='text-center text-xl my-2'>¿En qué puedo ayudarte?</p>
+            <div className='flex flex-col px-4 md:px-0 py-4 items-center justify-center'>
+              <div className='mx-3 p-1'><img src={mail} alt="" className='h-6 inline-block ' /> irenealcainealvarez@gmail.com</div>
+              <div className='mx-3 p-1'><img src={phone} alt="" className='h-6 inline-block' /> +34 629 88 21 29</div>
+            </div>
           </div>
-
 
           <form
             ref={form}
             action=""
-            className='flex justify-center flex-col items-center'
+            className='flex justify-center flex-col items-center md:w-3/5'
             onSubmit={sendEmail}
           >
-            <div className='flex flex-col w-5/6 md:w-4/6'>
-              <div className='flex flex-col md:flex-row items-center'>
-                <div className='w-full md:w-1/2 px-2'>
+            <div className='flex flex-col w-full'>
+              <div className='flex flex-col items-center'>
+                <div className='w-full'>
                   <input
                     id="name"
                     name="from_name"
                     type="text"
-                    placeholder="Nombre"
-                    className='bg-transparent focus:outline-none focus:ring-0 placeholder:text-violet-200 placeholder:opacity-50 border-0 border-b-2 border-violet-700 p-2 px-2 block w-full'
+                    className='bg-transparent outline-none ring-0 placeholder:text-violet-200 placeholder:opacity-50 border-0 border-b-2 border-violet-700 px-4 py-2 w-full focus:bg-violet-900 valid:bg-violet-900 valid:bg-opacity-50 focus:rounded-lg valid:rounded-lg transition-all duration-500 ease-in-out'
+                    placeholder='Nombre'
                     required
-                  ></input>
+                  >
+                  </input>
+
+
                 </div>
-                <div className='w-full md:w-1/2 px-2'>
+                <div className='w-full'>
                   <input
                     id="mail"
                     name="from_mail"
                     type="mail"
                     placeholder="Correo"
-                    className='bg-transparent focus:outline-none focus:ring-0 placeholder:text-violet-200 placeholder:opacity-50 border-0 border-b-2 border-violet-700 p-2 px-2 block w-full'
+                    className='bg-transparent outline-none ring-0 placeholder:text-violet-200 placeholder:opacity-50 border-0 border-b-2 border-violet-700 px-4 py-2 w-full focus:bg-violet-900 valid:bg-violet-900  valid:bg-opacity-50 focus:rounded-lg valid:rounded-lg transition-all duration-500 ease-in-out mt-4'
                     required
                   ></input>
                 </div>
 
               </div>
-              <input
+              <textarea
                 id="message"
                 name='message'
-                type="textarea"
                 placeholder="Mensaje"
-                className='bg-transparent placeholder:text-violet-200 placeholder:opacity-50 border rounded-lg focus:outline-none focus:ring-0 border-violet-700 px-4 py-2 my-4 h-20 '
+                className='bg-transparent placeholder:text-violet-200 placeholder:opacity-50 border rounded-lg outline-none border-violet-700 px-4 py-2 my-4 h-40 focus:bg-violet-900 valid:bg-violet-900 valid:bg-opacity-50'
                 required
-              ></input>
+              ></textarea>
 
               <button
                 className='border rounded-lg uppercase font-bold p-1 hover:bg-violet-900 bg-violet-700 transition-all duration-500 ease-in-out px-4 py-2'
