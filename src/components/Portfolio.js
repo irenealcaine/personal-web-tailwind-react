b155cc0531d2fbd9d79a9460f33517b512237054
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-31 23:41:26 +0100

    styled portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -17,7 +17,7 @@ const Portfolio = () => {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 2
+      items: 3
     },
     tablet: {
       breakpoint: { max: 1024, min: 640 },
@@ -34,7 +34,7 @@ const Portfolio = () => {
 
       <div className='md:min-h-screen flex flex-col' id='portfolio'>
         <h2 className='pt-20 text-center text-6xl md:text-8xl text-violet-700 font-caveat mb-8'>Proyectos</h2>
-        <div className='md:mt-8 mt-4 w-5/6 mx-auto'>
+        <div className='md:mt-8 mt-4 w-11/12 mx-auto'>
           <Carousel
             swipeable={true}
             responsive={responsive}
@@ -50,16 +50,16 @@ const Portfolio = () => {
               portfolioData.map((data, key) => {
                 return (
 
-                  <div key={key} className="mx-2">
+                  <div key={key} className="p-4 group">
                     <a href={data.url} target={'_blank'} rel="noreferrer">
-                      <img src={data.favicon} alt='img' className='w-16 absolute rounded bg-violet-700/20 p-1 border-violet-700 border-2' />
-                      <img src={data.img} alt="" className=' mx-auto my-2 shadow-lg rounded-md hover:scale-105 hover:cursor-pointer transition-all duration-500 ease-in-out' />
-                      <p className='text-center text-xl'>{data.title}</p>
-                    </a>
+                      <p className='text-start md:text-xl group-hover:underline decoration-violet-600'>{data.title}</p>
+                      <img src={data.favicon} alt='img' className='w-12 md:w-16 absolute rounded bg-violet-700/40 p-1 border-violet-700 border-2 z-50 group-hover:scale-75 transition-all duration-500 ease-in-out' />
+                      <img src={data.img} alt="" className=' mx-auto my-2 shadow-lg rounded-md group-hover:scale-105 cursor-pointer transition-all duration-500 ease-in-out' />
 
-                    <div className='flex justify-center mt-2'>
-                      {data.icons.map(s => (<img src={s} alt="" className='h-6 md:h-10 px-1' />))}
-                    </div>
+                      <div className='flex justify-center mt-2'>
+                        {data.icons.map(s => (<img src={s} alt="" className='h-6 md:h-10 md:px-2 px-1 group-hover:scale-75 transition-all duration-500 ease-in-out' />))}
+                      </div>
+                    </a>
                   </div>
                 )
               })
