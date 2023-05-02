56a152d38df78970540780df39c87e1e3ed255cd
ialcaine <irenealcainealvarez@gmail.com>
2023-05-02 08:21:09 +0000

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -32,56 +32,28 @@ const Portfolio = () => {
         <h2 className="pt-20 text-center text-6xl md:text-8xl text-white font-caveat mb-8">
           <span>Proyectos</span>
         </h2>
-        <div className="md:mt-8 mt-4 w-11/12 mx-auto">
-          <Carousel
-            swipeable={true}
-            responsive={responsive}
-            ssr={true}
-            infinite={true}
-            autoPlay={true}
-            autoPlaySpeed={2800}
-            customTransition="all 1.5s"
-            transitionDuration={1500}
-            // disableSlideInfo={false}
-          >
-            {portfolioData.map((data, key) => {
-              return (
-                <div key={key} className="py-4 md:px-8 group">
-                  <a href={data.url} target={"_blank"} rel="noreferrer">
-                    <p className="text-start md:text-xl group-hover:underline decoration-violet-600">
-                      {data.title}
-                    </p>
-                    <img
-                      src={data.favicon}
-                      alt="img"
-                      className="w-12 md:w-16 absolute rounded bg-violet-700/40 p-1 border-violet-700 border-2 z-50 group-hover:scale-75 transition-all duration-500 ease-in-out"
-                    />
-                    <a href={data.github} target={"_blank"} rel="noreferrer">
-                      <img
-                        src={github}
-                        alt="img"
-                        className="w-8 md:w-12 absolute rounded bottom-20 right-2 bg-violet-900/80 p-1 border-violet-700 border-2 z-50 transition-all duration-500 ease-in-out"
-                      />
-                    </a>
-                    <img
-                      src={data.img}
-                      alt=""
-                      className=" mx-auto my-2 shadow-lg rounded-md group-hover:scale-105 cursor-pointer transition-all duration-500 ease-in-out"
-                    />
-                    <div className="flex justify-center mt-2">
-                      {data.icons.map((s) => (
-                        <img
-                          src={s}
-                          alt=""
-                          className="h-6 md:h-10 md:px-2 px-1 group-hover:scale-75 transition-all duration-500 ease-in-out"
-                        />
-                      ))}
-                    </div>
-                  </a>
-                </div>
-              );
-            })}
-          </Carousel>
+        <div className="w-11/12 mx-auto flex flex-wrap gap-4 justify-center">
+          {portfolioData.map((data, key) => {
+            return (
+              <div
+                key={key}
+                className="w-full sm:w-5/12 md:w-3/12 group relative overflow-hidden border-2 border-purple-700 rounded-xl"
+              >
+                <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-500"></div>
+                <img
+                  src={data.favicon}
+                  alt=""
+                  className="w-full group-hover:scale-125 transition-all duration-500 "
+                />
+                <p className="p-6 absolute -bottom-full left-1 group-hover:bottom-24 transition-all duration-500 z-50">
+                  {data.title}
+                </p>
+                <p className="p-4 absolute -bottom-full left-1 group-hover:bottom-12 transition-all duration-500 z-50">
+                  {data.title}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
