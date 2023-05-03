74b267851300ddb6cce5ab07b56afc0c93903f05
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-05-03 17:20:09 +0200

    add projectDetails working

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -1,38 +1,59 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { portfolioData } from "../components/data/PortfolioData";
+import Button from "../components/Button";
 
 const ProjectDetails = () => {
-  //   const { id } = useParams();
-  //   portfolioData = portfolioData.id === id;
-
-  //   const [project, setProject] = useState({});
-
-  //   useEffect(() => {
-  //     setProject(portfolioData.id === id);
-  //   }, [id]);
-
-  //   useEffect(() => {
-  //     const selectedProject = portfolioData.find((project) => project.id === id);
-  //     setProject(selectedProject);
-  //   }, [id]);
-
-  //   const project = projects.find((p) => p.id === id);
 
   const { id } = useParams();
+  const project = portfolioData.find((project) => project.id === parseInt(id));
 
-  const project = portfolioData.find((project) => project.id === id);
   return (
     <div>
       <Header />
+      <Button
+        href="/portfolio"
+        text="Ver todos"
+        design="primary"
+        className="text-2xl mx-auto"
+      />
+      <div>
+        <h1>{project.title}</h1>
+        <img src={project.img} alt={project.title} />
+        <img src={project.favicon} alt={project.title} />
+        <p>Tech usadas:
 
-      <p>{project.title}</p>
+          {project.icons.map((icon) => {
+            return (
+              <img src={icon} alt="icon" className="h-8" />
+            )
+          })}
+        </p>
+        <p>
+          {project.description}
+        </p>
+        <Button
+          href={project.url}
+          text="Ir a la web"
+          design="primary"
+          className="text-2xl mx-auto"
+          target={"_blank"}
+          rel="noreferrer"
+        />
+        <Button
+          href={project.github}
+          text="Ir al repo"
+          design="primary"
+          className="text-2xl mx-auto"
+          target={"_blank"}
+          rel="noreferrer"
+        />
+      </div>
       <Contact />
       <Footer />
-    </div>
+    </div >
   );
 };
 
