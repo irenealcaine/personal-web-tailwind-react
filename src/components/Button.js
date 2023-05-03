2641c24db3058effe9fe213122364ba76744643f
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-03 09:58:16 +0000

    Update Button.js and index.css

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,15 @@ const Button = ({ href, text, design, className }) => {
   let designs = "";
   if (design === "primary") {
     designs =
-      "text-white hover:text-violet-900 bg-violet-900 hover:bg-white border-violet-900 ";
+      "shadow text-white bg-violet-900 hover:bg-transparent border-violet-900 ";
   } else {
     designs =
-      "text-violet-900 hover:text-white bg-white hover:bg-violet-900 border-white";
+      "shadow-secondary text-violet-900 hover:text-white bg-white hover:bg-transparent border-white";
   }
   return (
     <a
       href={href}
-      className={`${designs} shadow inline-block font-caveat text-xl text-center px-6 py-1 border-2 rounded-lg transition-all duration-500 ${className}`}
+      className={`${designs} inline-block font-caveat text-xl text-center px-6 py-1 border-2 rounded-lg transition-all duration-500 ${className}`}
     >
       {text}
     </a>
